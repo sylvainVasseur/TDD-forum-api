eee23c118830f27d66c7ad3bf3d009774ca4fca6
Sylvain.Vasseur <sylvainvasseur2@gmail.com>
2022-07-06 16:19:33 +0200

    program.cs

--- Repositories/TopicRepository.cs
+++ Repositories/TopicRepository.cs
@@ -4,14 +4,21 @@ namespace forum_api.Repositories
 {
     public class TopicRepository
     {
+        private forumdbContext _context;
+
+        public TopicRepository(forumdbContext context)
+        {
+            _context = context;
+        }
+
         public virtual Topic FindAll()
         {
-
+            _context.Topics.
         }
 
         public virtual Topic FindById(int id)
         {
-
+            _context.Topics.
         }
 
         public virtual Topic DeleteById(int id)

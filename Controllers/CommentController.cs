e394ec25ca7d0a5f811f030eeea5b1fc163946c1
Simon Gonzalez <gonzalezsimon@free.fr>
2022-07-07 09:32:52 +0200

    Ajout Controllers

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace forum_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommentController : ControllerBase
+    {
+        private forum_api db;
+
+        public CommentController(forum_api injectedContext)
+        {
+            db = injectedContext;
+        }
+
+
+    }
+}

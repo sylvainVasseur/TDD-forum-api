d3db167ecb21a615a1eaa43205ae2332428b9446
Simon Gonzalez <gonzalezsimon@free.fr>
2022-07-07 10:53:38 +0200

    Modif Controller

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+//using forum_api.Services;
+using Microsoft.AspNetCore.Http;
+
 
 namespace forum_api.Controllers
 {
@@ -8,11 +11,45 @@ namespace forum_api.Controllers
     {
         private forum_api db;
 
+        private readonly CommentService _services;
+
         public CommentController(forum_api injectedContext)
         {
             db = injectedContext;
         }
 
+        [HttpGet]
+        public IActionResult FindAll()
+        {
+            return Ok(this._service.FindAll());
+        }
 
+        [HttpGet("{id}")]
+        public IActionResult FindById(string id)
+        {
+            return Ok(this._service.FindById(id));
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Comment comment)
+        {
+            this._service.Create(comment);
+            return Ok("Created");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            this._service.DeleteById(id);
+            return Ok("Deleted");
+        }
+
+        [HttpPut]
+        public IActionResult Update(string id, Comment comment)
+        {
+            this._service.UpdateComment(id, comment);
+            return Ok("Updated");
+        }
     }
 }
--- Controllers/TopicController.cs
+++ Controllers/TopicController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+//using forum_api.Services;
+using Microsoft.AspNetCore.Http;
 
 namespace forum_api.Controllers
 {
@@ -8,11 +10,45 @@ namespace forum_api.Controllers
     {
         private forum_api db;
 
+        private readonly CommentService _services;
+
         public TopicController(forum_api injectedContext)
         {
             db = injectedContext;
         }
 
+        [HttpGet]
+        public IActionResult FindAll()
+        {
+            return Ok(this._service.FindAll());
+        }
 
+        [HttpGet("{id}")]
+        public IActionResult FindById(string id)
+        {
+            return Ok(this._service.FindById(id));
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Topic topic)
+        {
+            this._service.Create(topic);
+            return Ok("Created");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            this._service.DeleteById(id);
+            return Ok("Deleted");
+        }
+
+        [HttpPut]
+        public IActionResult Update(string id, Comment comment)
+        {
+            this._service.UpdateComment(id, comment);
+            return Ok("Updated");
+        }
     }
 }
